68a294e80b9afe17a2718ffb4fcb5cec42b4f047
ksinger12 <kylesinger@rogers.com>
2021-02-25 14:09:51 -0500

    Output file to txt

---
+++ output.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def __init__(self, file_name):
+        self.f = open(file_name, "a+")
+
+    def add_intersection(self, intersection, number_of_streets_green, street_names, street_times):
+        self.f.write(intersection)
+        self.f.write(number_of_streets_green)
+        for i in len(street_names):
+            self.f.write(street_names[i] + " " + street_times[i])
+
+    def close_file(self):
+        self.f.close()
