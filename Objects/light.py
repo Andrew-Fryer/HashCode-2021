9c871e452d656f4de5fbc1bf03bdf525276a6684
ksinger12 <kylesinger@rogers.com>
2021-02-25 13:27:00 -0500

    Adding objects for simulation

---
+++ Objects/light.py
@@ -0,0 +1,6 @@
+
+class Light:
+    def __init__(self, state, length, street_connections):
+        self.state = state
+        self.length = length
+        self.street_connection = street_connections
