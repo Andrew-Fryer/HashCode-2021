0a78d12f5742eb590dc11134ffc0b6b3cb68fd2f
Andrew Fryer <andrew.fryer@queensu.ca>
2021-02-25 14:24:23 -0500

    typo

--- output.py
+++ output.py
@@ -6,7 +6,7 @@ class Solution:
     def add_intersection(self, intersection, number_of_streets_green, street_names, street_times):
         self.f.write(intersection)
         self.f.write(number_of_streets_green)
-        for i in len(street_names):
+        for i in range(len(street_names)):
             self.f.write(street_names[i] + " " + street_times[i])
 
     def close_file(self):
